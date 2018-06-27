4c73d899c4eb449a5367e46933159ebf68dc2cf5
Yarin <yarinvak@gmail.com>
2018-06-27 16:17:02 +0300

    adjust to graphql-tools standard

--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
+import {GraphQLServer} from 'graphql-yoga'
 import {RunGraphQLServer} from '@enigmatis/polaris';
 import {Schema} from './schema/schema';
 RunGraphQLServer(Schema, 3000);
+
+// const server = new GraphQLServer({schema: Schema})
+// server.start(() => console.log('Server is running on localhost:4000'))
