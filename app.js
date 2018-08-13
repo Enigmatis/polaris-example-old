ffcdf0b5e34d84ba84f85b491024799aeefc80e3
guy120494 <guy120494@gmail.com>
2018-08-13 14:18:00 +0300

    Deleted compiled js files

--- app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var polaris_1 = require("@enigmatis/polaris");
-var schema_1 = require("./schema/schema");
-polaris_1.RunGraphQLServer(schema_1.Schema, 3000);
-//# sourceMappingURL=app.js.map
