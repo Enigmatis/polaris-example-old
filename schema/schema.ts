fef35c9918c7a9ba160b303429f87c96b81f3729
Yarin <yarinvak@gmail.com>
2018-08-22 14:17:11 +0300

    add more inversify features

--- schema/schema.ts
+++ schema/schema.ts
@@ -1,25 +1,29 @@
-import {InjectableResolver, InjectableType} from "@enigmatis/polaris";
-import {Container} from "inversify";
-import {buildProviderModule} from "inversify-binding-decorators";
 import {merge} from 'lodash';
 import Polaris = require("@enigmatis/polaris");
 import "reflect-metadata";
+import glob = require('glob');
+import path= require('path');
+import {ISchemaCreator} from '@enigmatis/polaris';
+import {Container} from "inversify";
+import {buildProviderModule} from "inversify-binding-decorators";
+// Require all types and resolvers so they can be injected later
+requireAllInFolder(path.join(__dirname, './entities/**/*'));
+requireAllInFolder(path.join(__dirname, './resolvers/**/*'));
 
-require('./entities/book');
-require('./entities/mutation');
-require('./entities/book.input');
-require('./entities/query');
-require('./resolvers/query');
-require('./resolvers/mutation');
-require('./resolvers/book');
+function requireAllInFolder(pathToDir: string): void {
+    let files = glob.sync(pathToDir);
+    files.forEach(function (file) {
+        file = file.replace(/\.[^/.]+$/, "");
+        require(file);
+    });
+}
 
+// Create container
 let container = new Container();
 container.load(buildProviderModule());
 
-let schemaDefinition = `schema {query: Query, mutation: Mutation}`;
-let definitions = [schemaDefinition, ...container.getAll<InjectableType>("InjectableType").map<string>(x => x.definition())];
-let resolvers = merge(container.getAll<InjectableResolver>("InjectableResolver").map(x =>
-    x.resolver()
-));
-let schema = new Polaris.PolarisTypeWrapper(definitions, resolvers);
-export default schema;
+let creator: ISchemaCreator = container.get<ISchemaCreator>("ISchemaCreator");
+
+let schema = creator.generateSchema();
+
+export default schema
