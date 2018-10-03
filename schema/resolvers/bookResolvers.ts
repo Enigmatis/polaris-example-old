2174dcb1fc84f061911df8dbae192556c631d210
osher-sade <osher.sade@gmail.com>
2018-10-03 10:42:42 +0300

    added basic request headers logic

--- schema/resolvers/bookResolvers.ts
+++ schema/resolvers/bookResolvers.ts
@@ -1,6 +1,9 @@
 let resolvers = {
     Book: {
-        title(book) {
+        title(book, _, context) {
+            if (context.edition) {
+                return context.edition + ': ' + book.title;
+            }
             return 'Special Edition: ' + book.title
         }
     }
