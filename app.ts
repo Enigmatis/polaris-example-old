cb2b5dc028b5abee798686b6b9c9da18f9d8304c
chenshoo <chenshooker@gmail.com>
2018-12-12 18:01:37 +0200

    integrate logger, using logger in example, creating logger in server

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
 import {PolarisGraphQLServer} from '@enigmatis/polaris';
-import {Schema} from './schema/schema';
-
+import { Schema } from './schema/schema';
+import { PolarisLogProperties } from '@enigmatis/polaris-logs';
 const props = {
     typeDefs: Schema.def,
-    resolvers: Schema.resolvers
+    resolvers: Schema.resolvers,
+    applicationLogProperties: {
+        id: "p01aris-10gs",
+        name: "polaris-example",
+        repositoryVersion: "v1",
+        environment: "dev",
+        component: "component"
+    }
 };
-
 let server = new PolarisGraphQLServer(props);
+server.getLogger().info(new PolarisLogProperties("hello world"));
 server.start();
 
 // const server = new GraphQLServer({schema: Schema})
