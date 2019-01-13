bf017cfd8669d2ce463c1d71459079d5b7870466
chenshoo <chenshooker@gmail.com>
2019-01-13 15:14:26 +0200

    config returns properties in example

--- ConfigExample.ts
+++ ConfigExample.ts
@@ -1,22 +1,54 @@
 import {injectable} from "inversify";
-import {IConfig} from "@enigmatis/polaris";
-import {PropertiesHolder} from "@enigmatis/polaris/dist/properties/propertiesHolder";
-import {readJsonFromFile} from "@enigmatis/polaris/dist/utils/FileReader";
+import {IConfig, LogProperties, PolarisProperties, readJsonFromFile} from "@enigmatis/polaris";
+
+
 const path = require('path');
+
 @injectable()
-export class Config implements IConfig{
-    properties: string;
-    logConfiguration: string;
-    constructor(){
+export class Config implements IConfig {
+
+    logstashHost: string;
+    logstashPort: number;
+    loggerLevel: string;
+
+    port: number;
+    endpoint: string;
+    applicationId: string;
+    applicationName: string;
+    repositoryVersion: string;
+    environment: string;
+    component: string;
+
+    constructor() {
         let polarisPropertiesPath = path.join(__dirname, '../properties.json');
-        this.properties = readJsonFromFile(polarisPropertiesPath);
+        this.initiallizePolarisProperties(readJsonFromFile(polarisPropertiesPath));
         let polarisLogConfigurationPath = path.join(__dirname, '../log-configuration.json');
-        this.logConfiguration = readJsonFromFile(polarisLogConfigurationPath);
+        this.initiallizeLogProperties(readJsonFromFile(polarisLogConfigurationPath));
     }
-    getProperties(){
-        return this.properties;
+
+    private initiallizeLogProperties(logConfiguration: string) {
+        this.loggerLevel = logConfiguration['loggerLevel'];
+        this.logstashHost = logConfiguration['logstashHost'];
+        this.logstashPort = logConfiguration['logstashPort'];
     }
-    getLogConfiguration(){
-        return this.logConfiguration;
+
+    private initiallizePolarisProperties(properties: string) {
+        this.port = properties['port'];
+        this.endpoint = properties['endpoint'];
+        this.applicationId = properties['applicationId'];
+        this.applicationName = properties['applicationName'];
+        this.repositoryVersion = properties['repositoryVersion'];
+        this.environment = properties['environment'];
+        this.component = properties['component'];
     }
+
+    public getLogProperties(): LogProperties {
+        return new LogProperties(this.loggerLevel, this.logstashHost, this.logstashPort);
+    }
+
+    public getPolarisProperties(): PolarisProperties {
+        return new PolarisProperties(this.port, this.endpoint, this.applicationId, this.applicationName,
+            this.repositoryVersion, this.environment, this.component);
+    }
+
 }
