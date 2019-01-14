8f4d68fd5a4d3e467e39fc3d42636529e4b597ec
chenshoo <chenshooker@gmail.com>
2019-01-14 11:27:25 +0200

    Merge branch 'master' of https://github.com/Enigmatis/polaris-logs
    # Conflicts:
    #	index.ts
    #	package-lock.json

--- app.ts
+++ app.ts
@@ -1,17 +1,9 @@
-import {ILogConfig, IPolarisGraphQLServer, IPolarisServerConfig} from '@enigmatis/polaris';
+import {ILogConfig, IPolarisGraphQLServer, IPolarisServerConfig, polarisContainer} from '@enigmatis/polaris';
 import {Container} from "inversify";
 import {schemaContainer} from "./schema/schema"
-import {polarisContainer} from "@enigmatis/polaris";
 import {ContextLogPropertiesWrapper} from "@enigmatis/polaris/dist/logging/ContextLogPropertiesWrapper";
-import {LogPropertiesWrapper} from "@enigmatis/polaris-logs";
 import {LogConfig} from "./config/LogConfig";
 import {PolarisServerConfig} from "./config/PolarisServerConfig";
-
-//TODO: delete these when log properties wrapper is deleted
-
-polarisContainer.bind<LogPropertiesWrapper>("LogPropertiesWrapper")
-    .toConstantValue(new ContextLogPropertiesWrapper())
-    .whenTargetNamed("GraphQLLogger");
 
 polarisContainer.bind<ILogConfig>("ILogConfig").to(LogConfig)
 polarisContainer.bind<IPolarisServerConfig>("IPolarisServerConfig").to(PolarisServerConfig)
