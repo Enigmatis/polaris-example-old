1233627c051b4089ed8a3a0577dbbe7d4251038a
chenshoo <chenshooker@gmail.com>
2019-01-28 12:06:49 +0200

    middlewares

--- middleware/example-middleware.ts
+++ middleware/example-middleware.ts
@@ -13,6 +13,7 @@ export class ExampleMiddleware implements PolarisMiddleware{
 
     postResolve(root: any, args:{ [argName: string]: any }, context: any, info: GraphQLResolveInfo , result:string) {
         //this.polarisLogger.debug(`from example after resolver, result: ${JSON.stringify(result)}`);
+        return result;
     }
 }
 /*
