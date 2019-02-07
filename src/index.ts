5feea79c5df683245d38c91eaac4ad38ef3d6970
bikov <bikov95@gmail.com>
2019-02-07 17:05:09 +0200

    add mongodb connection test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {
+    GraphqlLogger,
     GraphQLServer,
     LoggerConfig,
     POLARIS_TYPES,
@@ -7,10 +8,16 @@ import {
     PolarisServerConfig,
 } from '@enigmatis/polaris';
 import { Container } from 'inversify';
+
+import { config } from 'dotenv';
+
+config();
 import { ExampleLogConfig } from './config/example-log-config';
 import { ExampleServerConfig } from './config/example-server-config';
 import { ExampleMiddleware } from './middleware/example-middleware';
 import { schemaContainer } from './schema/schema';
+import { BookRepository } from './dal/book-repository';
+import { MongooseConnection } from '@enigmatis/mongo-driver';
 
 polarisContainer.bind<LoggerConfig>(POLARIS_TYPES.LoggerConfig).to(ExampleLogConfig);
 polarisContainer
@@ -21,3 +28,23 @@ const mergedContainer = Container.merge(polarisContainer, schemaContainer);
 const server: GraphQLServer = mergedContainer.get<GraphQLServer>(POLARIS_TYPES.GraphQLServer);
 
 server.start();
+
+const testDB = async () => {
+    const logger = mergedContainer.get<GraphqlLogger>(POLARIS_TYPES.GraphqlLogger);
+    const connectionString =
+        process.env.MONGO_CONNECTION_STRING;
+    if (connectionString) {
+        const mongoConnection = new MongooseConnection(
+            { connectionString, waitUntilReconnectInMs: 5000 },
+            logger,
+        );
+        await mongoConnection.initConnection();
+        const repository = new BookRepository();
+        logger.info(JSON.stringify(await repository.findAll(12, true), null, 4));
+        await mongoConnection.closeConnection();
+    } else {
+        logger.error(`enviorment variable 'MONGO_CONNECTION_STRING' not found`);
+    }
+};
+
+testDB();
--- src/middleware/example-middleware.ts
+++ src/middleware/example-middleware.ts
@@ -12,10 +12,14 @@ export class ExampleMiddleware implements PolarisMiddleware {
     @inject(POLARIS_TYPES.GraphqlLogger) logger!: GraphqlLogger;
 
     preResolve({ args, context }: MiddlewareParams) {
-        this.logger.debug(`from example before resolver, args: ${JSON.stringify(args)}`, { context });
+        this.logger.debug(`from example before resolver, args: ${JSON.stringify(args)}`, {
+            context,
+        });
     }
 
     postResolve({ result, context }: PostMiddlewareParams): void {
-        this.logger.debug(`from example after resolver, result: ${JSON.stringify(result)}`, { context });
+        this.logger.debug(`from example after resolver, result: ${JSON.stringify(result)}`, {
+            context,
+        });
     }
 }
